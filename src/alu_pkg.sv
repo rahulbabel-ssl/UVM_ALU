4473a78166c2c129bc03f832a9e09bf79d12140d
rahulbabel-ssl <rahulbabel14@gmail.com>
2025-08-22 11:50:34 +0530

    Create alu_pkg.sv

---
+++ src/alu_pkg.sv
@@ -0,0 +1,16 @@
+package alu_pkg;
+        import std::*;
+        import uvm_pkg::*;
+        `include "defines.svh"
+        `include "uvm_macros.svh"
+        `include "sequence_item.sv"
+        `include "sequence.sv"
+        `include "sequencer.sv"
+        `include "driver.sv"
+        `include "monitor.sv"
+        `include "agent.sv"
+        `include "scoreboard.sv"
+        // `include "coverage.sv"
+        `include "environment.sv"
+        `include "test.sv"
+endpackage
